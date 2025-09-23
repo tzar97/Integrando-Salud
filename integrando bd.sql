5e6c2abe2b77436be5a69b2e4059b46dc7bebc7a
tzar97 <65732142+tzar97@users.noreply.github.com>
2025-09-23 09:39:14 -0300

    Update integrando bd.sql

--- integrando bd.sql
+++ integrando bd.sql
@@ -14,15 +14,85 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Volcando estructura de base de datos para integrando_salud
+CREATE DATABASE IF NOT EXISTS `integrando_salud` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `integrando_salud`;
+
+-- Volcando estructura para tabla integrando_salud.cache
+CREATE TABLE IF NOT EXISTS `cache` (
+  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expiration` int NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla integrando_salud.cache: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla integrando_salud.cache_locks
+CREATE TABLE IF NOT EXISTS `cache_locks` (
+  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `owner` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expiration` int NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla integrando_salud.cache_locks: ~0 rows (aproximadamente)
 
+-- Volcando estructura para tabla integrando_salud.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla integrando_salud.failed_jobs: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla integrando_salud.jobs
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `attempts` tinyint unsigned NOT NULL,
+  `reserved_at` int unsigned DEFAULT NULL,
+  `available_at` int unsigned NOT NULL,
+  `created_at` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `jobs_queue_index` (`queue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla integrando_salud.jobs: ~0 rows (aproximadamente)
 
+-- Volcando estructura para tabla integrando_salud.job_batches
+CREATE TABLE IF NOT EXISTS `job_batches` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_jobs` int NOT NULL,
+  `pending_jobs` int NOT NULL,
+  `failed_jobs` int NOT NULL,
+  `failed_job_ids` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `options` mediumtext COLLATE utf8mb4_unicode_ci,
+  `cancelled_at` int DEFAULT NULL,
+  `created_at` int NOT NULL,
+  `finished_at` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla integrando_salud.job_batches: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla integrando_salud.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla integrando_salud.migrations: ~0 rows (aproximadamente)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -32,6 +102,20 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(4, '2025_09_16_120424_create_tipo_pets_table', 1),
 	(5, '2025_09_17_120424_create_pacientes_table', 1),
 	(6, '2025_09_18_120425_create_tratamientos_table', 1);
+
+-- Volcando estructura para tabla integrando_salud.pacientes
+CREATE TABLE IF NOT EXISTS `pacientes` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sexo` enum('M','F','X') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dni` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pacientes_dni_unique` (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla integrando_salud.pacientes: ~6 rows (aproximadamente)
 INSERT INTO `pacientes` (`id`, `nombre`, `apellido`, `sexo`, `dni`, `fecha_nacimiento`, `created_at`, `updated_at`) VALUES
@@ -43,13 +127,51 @@ INSERT INTO `pacientes` (`id`, `nombre`, `apellido`, `sexo`, `dni`, `fecha_nacim
 	(7, 'monito', 'fernandez', 'M', '1233345', '2001-12-24', '2025-09-20 23:48:48', '2025-09-20 23:48:48'),
 	(8, 'julia', 'benitez', 'F', '44445', '1992-06-05', '2025-09-20 23:49:54', '2025-09-20 23:49:54');
 
+-- Volcando estructura para tabla integrando_salud.password_reset_tokens
+CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla integrando_salud.password_reset_tokens: ~0 rows (aproximadamente)
 
--- Volcando datos para la tabla integrando_salud.sessions: ~3 rows (aproximadamente)
+-- Volcando estructura para tabla integrando_salud.sessions
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint unsigned DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sessions_user_id_index` (`user_id`),
+  KEY `sessions_last_activity_index` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla integrando_salud.sessions: ~4 rows (aproximadamente)
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
 	('rjbnvnW1j2mJgkTEpKUQfBHBT6sEmvMsPCIFfIiD', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoia29Nak96MkFzTUNqbjc2bEtielBEekRUTDFBNEYyMHVja3V4WlFwNSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wZXQvbGlzdGEiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1758475117),
 	('tnqkwJiAX5WqflBfe7i8OT9a0lYTN14HBRRuqMw5', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiQzZnelA2bThmMFZGMXJYbkZ6VXJkYzhSWnMzaGpwTk85RUVTQ0V4TyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fX0=', 1758411772),
-	('Xu8o6CXjaNJcsy7FXgHstPuMR2TJu8EuxuQeCmZu', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiZ2h3WlVjbkJ6U2Fzc1MxS3pxWGlYeVFFbERJeEhBR1E0SDBZN0NVVSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wYWNpZW50ZXMvbnVldm8iO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1758401394);
+	('Xu8o6CXjaNJcsy7FXgHstPuMR2TJu8EuxuQeCmZu', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiZ2h3WlVjbkJ6U2Fzc1MxS3pxWGlYeVFFbERJeEhBR1E0SDBZN0NVVSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wYWNpZW50ZXMvbnVldm8iO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1758401394),
+	('zOWSLGBz2nhBdslRMPVDeoSO5aQIj1xz6waO5MSq', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiWlBSaGZWeXExWEJCZmZGWlZ5STdDaXNSUlJRNWVRT3pqaEFIR3JRRCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDY6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wYWNpZW50ZXMvOC90cmF0YW1pZW50b3MiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1758578346);
+
+-- Volcando estructura para tabla integrando_salud.tipo_pets
+CREATE TABLE IF NOT EXISTS `tipo_pets` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color` enum('verde','amarillo','ambar','rojo') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `intensidad` smallint unsigned NOT NULL,
+  `duracion_min` smallint unsigned NOT NULL,
+  `requiere_ayuno` tinyint(1) NOT NULL DEFAULT '0',
+  `observaciones` text COLLATE utf8mb4_unicode_ci,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla integrando_salud.tipo_pets: ~4 rows (aproximadamente)
 INSERT INTO `tipo_pets` (`id`, `nombre`, `color`, `intensidad`, `duracion_min`, `requiere_ayuno`, `observaciones`, `estado`, `created_at`, `updated_at`, `deleted_at`) VALUES
@@ -58,9 +180,38 @@ INSERT INTO `tipo_pets` (`id`, `nombre`, `color`, `intensidad`, `duracion_min`, 
 	(5, 'Pulmon', 'ambar', 7, 23, 1, 'no coca', 1, '2025-09-20 17:28:04', '2025-09-20 17:28:04', NULL),
 	(6, 'Prostata', 'rojo', 8, 12, 1, 'no sex', 1, '2025-09-20 21:16:52', '2025-09-20 21:16:52', NULL);
 
+-- Volcando estructura para tabla integrando_salud.tratamientos
+CREATE TABLE IF NOT EXISTS `tratamientos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `paciente_id` bigint unsigned NOT NULL,
+  `tipo_pet_id` bigint unsigned NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tratamientos_paciente_id_foreign` (`paciente_id`),
+  KEY `tratamientos_tipo_pet_id_foreign` (`tipo_pet_id`),
+  CONSTRAINT `tratamientos_paciente_id_foreign` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `tratamientos_tipo_pet_id_foreign` FOREIGN KEY (`tipo_pet_id`) REFERENCES `tipo_pets` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla integrando_salud.tratamientos: ~1 rows (aproximadamente)
 INSERT INTO `tratamientos` (`id`, `paciente_id`, `tipo_pet_id`, `fecha_inicio`, `created_at`, `updated_at`) VALUES
 	(6, 2, 1, '2025-09-20', '2025-09-20 23:02:38', '2025-09-20 23:02:38');
+
+-- Volcando estructura para tabla integrando_salud.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla integrando_salud.users: ~0 rows (aproximadamente)
 
